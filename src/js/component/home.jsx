247fa62ed93f8cc104630b6867b078bad8b42b10
Auricel <148658628+Auricel@users.noreply.github.com>
2024-08-25 15:10:12 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const inicialLightColors = ["rojo", "amarillo", "verde"];
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
+	
+	const [lightColors, setLightColors] = useState(inicialLightColors);
+	const [lightColor, setLightColor] = useState("rojo");
+
+	//Secuencia para el cambio de colores
+	const cycleLightColor = () => {
+		if (lightColor === "rojo") {
+			setLightColor("amarillo");
+		} else if (lightColor === "amarillo") {
+			setLightColor("verde");
+		} else if (lightColor === "verde" && lightColors.includes("violeta")) {
+			setLightColor("violeta");
+		} else if (lightColor === "verde" && !lightColors.includes("violeta") || lightColor === "violeta") {
+			setLightColor("rojo");
+		}
+	};
+			
+		
+
+	const addvioletaLight = () => {
+		if (lightColors.includes("violeta")) {
+			setLightColors(["rojo", "amarillo", "verde"])
+		} else {
+			setLightColors([...lightColors, "violeta"])
+		}
+	};
+
+	return (
+		<div className="container">
+			<div className="seleccionador"></div>
+			<div className="caja-de-colores">
+				<div onClick={() => setLightColor("rojo")} className={"rojoLight light " + (lightColor === "rojo" ? "lightColorRojo" : "")}></div>
+				<div onClick={() => setLightColor("amarillo")} className={"amarilloLight light " + (lightColor === "amarillo" ? "lightColorAmarillo" : "")}></div>
+				<div onClick={() => setLightColor("verde")} className={"verdeLight light " + (lightColor === "verde" ? "lightColorVerde" : "")}></div>
+				{lightColors.includes("violeta") && (
+					<div onClick={() => setLightColor("violeta")} className={"violetaLight light " + (lightColor === "violeta" ? "lightColorVioleta" : "")}></div>
+				)}
+			</div>
+			<div>
+				<button className="btn mt-5 btn-danger" onClick={addvioletaLight}>Agregar/quitar violeta</button>
+				<button className="btn mt-5 btn-danger" onClick={cycleLightColor}>Siguiente color</button>
+			</div>
+		</div>
+	)
+};
+	
 export default Home;
