3fe4e3b4fe114b56d3e33dded84e81af82b7af3c
Auricel <148658628+Auricel@users.noreply.github.com>
2024-08-26 00:12:15 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,8 +4,8 @@ const inicialLightColors = ["rojo", "amarillo", "verde"];
 
 const Home = () => {
 
-	
-	const [lightColors, setLightColors] = useState(inicialLightColors);
+
+
 	const [lightColor, setLightColor] = useState("rojo");
 
 	//Secuencia para el cambio de colores
@@ -14,20 +14,8 @@ const Home = () => {
 			setLightColor("amarillo");
 		} else if (lightColor === "amarillo") {
 			setLightColor("verde");
-		} else if (lightColor === "verde" && lightColors.includes("violeta")) {
-			setLightColor("violeta");
-		} else if (lightColor === "verde" && !lightColors.includes("violeta") || lightColor === "violeta") {
+		} else if (lightColor === "verde") {
 			setLightColor("rojo");
-		}
-	};
-			
-		
-
-	const addvioletaLight = () => {
-		if (lightColors.includes("violeta")) {
-			setLightColors(["rojo", "amarillo", "verde"])
-		} else {
-			setLightColors([...lightColors, "violeta"])
 		}
 	};
 
@@ -38,16 +26,13 @@ const Home = () => {
 				<div onClick={() => setLightColor("rojo")} className={"rojoLight light " + (lightColor === "rojo" ? "lightColorRojo" : "")}></div>
 				<div onClick={() => setLightColor("amarillo")} className={"amarilloLight light " + (lightColor === "amarillo" ? "lightColorAmarillo" : "")}></div>
 				<div onClick={() => setLightColor("verde")} className={"verdeLight light " + (lightColor === "verde" ? "lightColorVerde" : "")}></div>
-				{lightColors.includes("violeta") && (
-					<div onClick={() => setLightColor("violeta")} className={"violetaLight light " + (lightColor === "violeta" ? "lightColorVioleta" : "")}></div>
-				)}
+
 			</div>
 			<div>
-				<button className="btn mt-5 btn-danger" onClick={addvioletaLight}>Agregar/quitar violeta</button>
 				<button className="btn mt-5 btn-danger" onClick={cycleLightColor}>Siguiente color</button>
 			</div>
 		</div>
 	)
 };
-	
+
 export default Home;
